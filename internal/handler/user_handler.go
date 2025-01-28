b7de6504c54fad083f5ba1d8f7e5679ef4a8bc73
Vivek Ajbe <vajbe.tce@gmail.com>
2025-01-28 21:59:07 +0530

    dummy add user

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,6 +1,9 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type UserHandler struct{}
 
@@ -9,7 +12,7 @@ func NewUserHandler() *UserHandler {
 }
 
 func (h *UserHandler) AddUser(w http.ResponseWriter, r *http.Request) {
-
+	log.Print("In Add user")
 }
 
 func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
